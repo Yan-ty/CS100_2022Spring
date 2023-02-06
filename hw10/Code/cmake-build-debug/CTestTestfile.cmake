e94379556c2e38164cf1d8d6493448f09647d060
Yanty <yanty@shanghaitech.edu.cn>
2023-02-06 18:54:54 +0800

    hw10

---
+++ hw10/Code/cmake-build-debug/CTestTestfile.cmake
@@ -0,0 +1,8 @@
+# CMake generated Testfile for 
+# Source directory: D:/YAN Tianyao/ShanghaiTech/Course/SIST/12 CS100 Introduction to Programming/Homework/hw10/Code
+# Build directory: D:/YAN Tianyao/ShanghaiTech/Course/SIST/12 CS100 Introduction to Programming/Homework/hw10/Code/cmake-build-debug
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+subdirs("src")
+subdirs("test")
---
+++ hw10/Code/cmake-build-debug/test/CTestTestfile.cmake
@@ -0,0 +1,14 @@
+# CMake generated Testfile for 
+# Source directory: D:/YAN Tianyao/ShanghaiTech/Course/SIST/12 CS100 Introduction to Programming/Homework/hw10/Code/test
+# Build directory: D:/YAN Tianyao/ShanghaiTech/Course/SIST/12 CS100 Introduction to Programming/Homework/hw10/Code/cmake-build-debug/test
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+add_test(unittest_polynomial_1 "test_polynomial" "D:/YAN Tianyao/ShanghaiTech/Course/SIST/12 CS100 Introduction to Programming/Homework/hw10/Code/data/p1.txt")
+set_tests_properties(unittest_polynomial_1 PROPERTIES  PASS_REGULAR_EXPRESSION "2.25" _BACKTRACE_TRIPLES "D:/YAN Tianyao/ShanghaiTech/Course/SIST/12 CS100 Introduction to Programming/Homework/hw10/Code/test/CMakeLists.txt;11;add_test;D:/YAN Tianyao/ShanghaiTech/Course/SIST/12 CS100 Introduction to Programming/Homework/hw10/Code/test/CMakeLists.txt;0;")
+add_test(unittest_polynomial_2 "test_polynomial" "D:/YAN Tianyao/ShanghaiTech/Course/SIST/12 CS100 Introduction to Programming/Homework/hw10/Code/data/p2.txt")
+set_tests_properties(unittest_polynomial_2 PROPERTIES  PASS_REGULAR_EXPRESSION "0.75" _BACKTRACE_TRIPLES "D:/YAN Tianyao/ShanghaiTech/Course/SIST/12 CS100 Introduction to Programming/Homework/hw10/Code/test/CMakeLists.txt;12;add_test;D:/YAN Tianyao/ShanghaiTech/Course/SIST/12 CS100 Introduction to Programming/Homework/hw10/Code/test/CMakeLists.txt;0;")
+add_test(unittest_pol_func_1 "test_polynomial_function" "D:/YAN Tianyao/ShanghaiTech/Course/SIST/12 CS100 Introduction to Programming/Homework/hw10/Code/data/")
+set_tests_properties(unittest_pol_func_1 PROPERTIES  PASS_REGULAR_EXPRESSION "3" _BACKTRACE_TRIPLES "D:/YAN Tianyao/ShanghaiTech/Course/SIST/12 CS100 Introduction to Programming/Homework/hw10/Code/test/CMakeLists.txt;17;add_test;D:/YAN Tianyao/ShanghaiTech/Course/SIST/12 CS100 Introduction to Programming/Homework/hw10/Code/test/CMakeLists.txt;0;")
+add_test(unittest_lambda_func_1 "test_lambda_function" "D:/YAN Tianyao/ShanghaiTech/Course/SIST/12 CS100 Introduction to Programming/Homework/hw10/Code/data/")
+set_tests_properties(unittest_lambda_func_1 PROPERTIES  PASS_REGULAR_EXPRESSION "1.6875" _BACKTRACE_TRIPLES "D:/YAN Tianyao/ShanghaiTech/Course/SIST/12 CS100 Introduction to Programming/Homework/hw10/Code/test/CMakeLists.txt;21;add_test;D:/YAN Tianyao/ShanghaiTech/Course/SIST/12 CS100 Introduction to Programming/Homework/hw10/Code/test/CMakeLists.txt;0;")
